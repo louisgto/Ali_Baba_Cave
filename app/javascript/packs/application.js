9d1a7b17965c00fa9ae9fdf4b7c8d70bdd45109e
Louis Deboutin <louis.deboutin@gmail.com>
2019-11-29 16:23:14 +0100

    azer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,12 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { loadDynamicBannerText } from '../components/banner';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initParticles } from '../plugins/init_particles';
+
+
+if (document.querySelector('#particles-js')) {
+  initParticles();
+}
 
 if (document.querySelector('#banner-typed-text')) {
   loadDynamicBannerText();
